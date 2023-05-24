b5f9c8631d6afea0f92f4d248590b4607d0a1a87
iddokip <kiptarusiddo@gmail.com>
2023-05-24 00:50:53 -0700

    Handle_pipe

--- today/shell.h
+++ today/shell.h
@@ -23,5 +23,6 @@ int comp_string(const char *str1, const char *str2);
 char *Append_Path(const char *dir, const char *cmnd);
 char *modfypath(const char *c_path, const char *cmnd, const char *args);
 char *Handle_Path(char **args);
+char* Handle_Pipe(char* arg);
 
 #endif
