8dd18aaefb3828b5b51084a9c6ba85c350b31df9
iddokip <kiptarusiddo@gmail.com>
2023-05-24 04:02:06 -0700

    ..

--- today/Handle_pipe.c
+++ today/Handle_pipe.c
@@ -12,16 +12,18 @@ char *Handle_Pipe(char *arg)
 	char *cmnd = strtok(arg, "|");
 	char *arg_str = strtok(NULL, "|");
 	char *trimmed_args = arg_str;
+	size_t len;
+	char *modified_path;
 
 	while (*trimmed_args == ' ')
 		trimmed_args++;
 
-	size_t len = getLength(trimmed_args);
+	len = getLength(trimmed_args);
 
 	while (len > 0 && trimmed_args[len - 1] == ' ')
 		trimmed_args[--len] = '\0';
 
-	char *modified_path = malloc(getLength(cmnd) + getLength(trimmed_args) + 4);
+	modified_path = malloc(getLength(cmnd) + getLength(trimmed_args) + 4);
 
 		if (modified_path == NULL)
 		{
